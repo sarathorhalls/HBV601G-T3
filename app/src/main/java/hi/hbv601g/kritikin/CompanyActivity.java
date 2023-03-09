487558d34b4557e2b1c3c7636e24118fbe479c9d
Reyn Alpha <reynalpha@outlook.com>
2023-03-09 17:36:31 +0000

    Initial work on company view layout

---
+++ app/src/main/java/hi/hbv601g/kritikin/CompanyActivity.java
@@ -0,0 +1,14 @@
+package hi.hbv601g.kritikin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company);
+    }
+}
