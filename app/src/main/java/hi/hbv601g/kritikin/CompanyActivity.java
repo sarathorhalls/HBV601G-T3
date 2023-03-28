908ab8ef158ad42064d26c875248a646328186ba
Reyn Alpha <reynalpha@outlook.com>
2023-03-28 21:11:13 +0000

    Initial work on displaying company data programmatically

--- app/src/main/java/hi/hbv601g/kritikin/CompanyActivity.java
+++ app/src/main/java/hi/hbv601g/kritikin/CompanyActivity.java
@@ -3,12 +3,51 @@ package hi.hbv601g.kritikin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.android.material.chip.Chip;
+
+import hi.hbv601g.kritikin.services.NetworkManagerService;
+import hi.hbv601g.kritikin.services.implementation.NetworkManagerServiceImplementation;
 
 public class CompanyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get extras
+        Bundle extras = getIntent().getExtras();
+        int companyId = extras.getInt("companyId");
+
+        // Get company info from API
+        // NetworkManagerService net = new NetworkManagerServiceImplementation();
+        // String companyJson = net.doGET("/company/1", null);
+
+        // Set content view
         setContentView(R.layout.activity_company);
+
+        // Get components
+        TextView companyNameText = (TextView) findViewById(R.id.companyNameText);
+        Chip companyWebsiteChip = (Chip) findViewById(R.id.companyWebsiteChip);
+        Chip companyPhoneChip = (Chip) findViewById(R.id.companyPhoneChip);
+        Chip companyOpeningHoursChip = (Chip) findViewById(R.id.companyOpeningHoursChip);
+        Chip companyAddressChip = (Chip) findViewById(R.id.companyAddressChip);
+        RatingBar companyRatingBar = (RatingBar) findViewById(R.id.companyRatingBar);
+        TextView companyDescriptionText = (TextView) findViewById(R.id.companyDescriptionText);
+        TextView noReviewsText = (TextView) findViewById(R.id.noReviewsText);
+        View reviewsView = findViewById(R.id.reviewsView);
+        Button writeReviewButton = (Button) findViewById(R.id.writeReviewButton);
+        TextView noQuestionsText = (TextView) findViewById(R.id.noQuestionsText);
+        Button askQuestionButton = (Button) findViewById(R.id.askQuestionButton);
+        Button requestAdminAccessButton = (Button) findViewById(R.id.requestAdminAccessButton);
+
+        // Set company info
+        companyNameText.setText("Company id: " + companyId);
+        reviewsView.setVisibility(View.VISIBLE);
+        noReviewsText.setVisibility(View.GONE);
     }
 }
--- app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
+++ app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
@@ -2,6 +2,7 @@ package hi.hbv601g.kritikin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: remove test code
+        Intent companyIntent = new Intent(MainActivity.this, CompanyActivity.class);
+        companyIntent.putExtra("companyId", 1);
+        startActivity(companyIntent);
     }
 }
