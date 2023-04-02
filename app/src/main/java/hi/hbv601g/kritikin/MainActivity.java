207e6a675a46d51706e992aab33fe9716b772a25
Reyn Alpha <reynalpha@outlook.com>
2023-04-02 21:39:05 +0000

    Implement search

--- app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
+++ app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
@@ -7,10 +7,30 @@ import android.os.Bundle;
 import android.widget.SearchView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import hi.hbv601g.kritikin.entities.Company;
+import hi.hbv601g.kritikin.services.CompanyService;
+import hi.hbv601g.kritikin.services.implementation.CompanyServiceImplementation;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Company> searchResults;
+    private CompanyService companyService;
+    private SearchView searchView;
+    private RecyclerView searchResultRecycler;
+
+    /**
+     * Searches for companies matching query from the web service and displays the results
+     * @param query Query to search for
+     */
+    private void searchForCompanies(String query) {
+        new Thread(() -> {
+            // Get company list from service and store in instance variable
+            searchResults = companyService.findByName(query);
+            // Display search results on UI thread
+            MainActivity.this.runOnUiThread(() -> searchResultRecycler.setAdapter(new CompanyAdapter(searchResults)));
+        }).start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,48 +38,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get search view and results list
-        SearchView searchView = (SearchView) findViewById(R.id.searchView);
-        RecyclerView searchResultRecycler = (RecyclerView) findViewById(R.id.searchResultRecycler);
+        searchView = (SearchView) findViewById(R.id.searchView);
+        searchResultRecycler = (RecyclerView) findViewById(R.id.searchResultRecycler);
 
-        // Set search results adapter
-        ArrayList<Company> searchResults = new ArrayList<>();
-        searchResultRecycler.setAdapter(new CompanyAdapter(searchResults));
+        // Set empty adapter for recycler view to work
+        searchResultRecycler.setAdapter(new CompanyAdapter(new ArrayList<>()));
 
         // Handle search
+        companyService = new CompanyServiceImplementation();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                // TODO: implement actual search
-                searchResults.add(new Company(
-                        5L,
-                        "Test Company",
-                        4.5,
-                        "https://example.org",
-                        5555555,  // FIXME: phone number should not be an integer
-                        "This is a company description",
-                        "Hagatorg 1",
-                        "10:00–12:00",
-                        null,
-                        null
-                ));
-                searchResults.add(new Company(
-                        6L,
-                        "Test Company 2",
-                        3.0,
-                        "https://example.org",
-                        5555555,  // FIXME: phone number should not be an integer
-                        "This is a company description",
-                        "Hagatorg 1",
-                        "10:00–12:00",
-                        null,
-                        null
-                ));
+                searchForCompanies(s);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
-                searchResults.clear();
+                if (searchResults != null) {
+                    searchResults.clear();
+                }
                 return false;
             }
         });
