c36734cc15b05a97876d6903d8e39c54a87e1d39
Reyn Alpha <reynalpha@outlook.com>
2023-04-02 14:58:54 +0000

    Clear search results on query text change

--- app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
+++ app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String s) {
+                searchResults.clear();
                 return false;
             }
         });
