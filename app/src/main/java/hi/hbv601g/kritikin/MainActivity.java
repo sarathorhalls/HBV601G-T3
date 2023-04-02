2bc36f857f6ac3c1f0e6abad88f6ccafd0b5011f
Reyn Alpha <reynalpha@outlook.com>
2023-04-02 14:25:55 +0000

    Add search UI

--- app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
+++ app/src/main/java/hi/hbv601g/kritikin/MainActivity.java
@@ -25,36 +25,35 @@ public class MainActivity extends AppCompatActivity {
         SearchView searchView = (SearchView) findViewById(R.id.searchView);
         RecyclerView searchResultRecycler = (RecyclerView) findViewById(R.id.searchResultRecycler);
         ArrayList<Company> searchResults = new ArrayList<>();
-        searchResults.add(new Company(
-                5L,
-                "Test Company",
-                4.5,
-                "https://example.org",
-                5555555,  // FIXME: phone number should not be an integer
-                "This is a company description",
-                "Hagatorg 1",
-                "10:00–12:00",
-                null,
-                null
-        ));
-        searchResults.add(new Company(
-                6L,
-                "Test Company 2",
-                3.0,
-                "https://example.org",
-                5555555,  // FIXME: phone number should not be an integer
-                "This is a company description",
-                "Hagatorg 1",
-                "10:00–12:00",
-                null,
-                null
-        ));
         searchResultRecycler.setAdapter(new CompanyAdapter(searchResults));
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-
+                searchResults.add(new Company(
+                        5L,
+                        "Test Company",
+                        4.5,
+                        "https://example.org",
+                        5555555,  // FIXME: phone number should not be an integer
+                        "This is a company description",
+                        "Hagatorg 1",
+                        "10:00–12:00",
+                        null,
+                        null
+                ));
+                searchResults.add(new Company(
+                        6L,
+                        "Test Company 2",
+                        3.0,
+                        "https://example.org",
+                        5555555,  // FIXME: phone number should not be an integer
+                        "This is a company description",
+                        "Hagatorg 1",
+                        "10:00–12:00",
+                        null,
+                        null
+                ));
                 return false;
             }
 
